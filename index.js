1c406ab0636535df62247e5c137f069eaa42162e
Immanvr46 <111418836+Immanvr46@users.noreply.github.com>
2023-02-14 03:04:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express =require('express');
 const db=require('./db/connect');
-constcors=require('cors')
+const cors=require('cors')
 // import routes
 const employeeRoutes=require('./routes/employees.routes');
 
